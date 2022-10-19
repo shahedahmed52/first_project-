681810c37840e5a92c943480bebcc422103d6fcc
unknown <shahedmetwaly2004@gmail.com>
2022-10-19 15:50:58 +0200

    c++

---
+++ shahed.cpp
@@ -0,0 +1,15 @@
+#include <iostream >
+#include <string> 
+using namespace std;
+int main ()
+{
+
+	string arr[3]={"assuit " , "demyat " , "cairo"};
+	for (int i=0 ;i<3;i++)
+	{
+		cout << arr[i] << endl; 
+	}
+	system("pause ");
+	return 0; 
+
+}
